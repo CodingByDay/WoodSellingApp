f19f8ffb06df157cd2f284c53a9001dd3c06ce7f
hanumansitaram <hanuman@unv.rs>
2020-09-20 18:23:40 +0200

    Adding db changes, makin forms

--- main/models.py
+++ main/models.py
@@ -17,16 +17,23 @@ class Item(models.Model):
 
 ######################################################################################
 class Offer(models.Model):
-    item                         =models.ForeignKey(Item, on_delete=models.CASCADE)
+    item                         =models.ForeignKey(Item, on_delete=models.CASCADE, null=False)
+    quantity1                     =models.IntegerField(null=False)   
+    second                       =models.ForeignKey(Item, on_delete=models.CASCADE, related_name='second', null=True)
+    quantity2                     =models.IntegerField(null=True)
+    third                        =models.ForeignKey(Item, on_delete=models.CASCADE, related_name='third', null=True)
+    quantity3                    =models.IntegerField(null=True)
+    fourth                       =models.ForeignKey(Item, on_delete=models.CASCADE, related_name='fourth', null=True)
+    quantity4                     =models.IntegerField(null=True)
     first_last_name              =models.CharField(max_length=120)
-    quantity                     =models.IntegerField()   
+     
     phone_number                 =models.IntegerField() 
     email_address                =models.EmailField(null=False)   
     address                      =models.CharField(max_length=120)
     datetime                     =models.DateTimeField()
     finalOffer                   =models.IntegerField()
     sendEmail                    =models.BooleanField(default=False)
-
+    status                       =models.BooleanField(default=False) 
     def __str__(self):
         return self.first_last_name
     
