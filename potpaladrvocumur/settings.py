f87f54edd816493498bb5fc4da4f7b60bbd49fc3
hanumansitaram <hanuman@unv.rs>
2020-10-06 01:22:27 +0200

    last

--- potpaladrvocumur/settings.py
+++ potpaladrvocumur/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = "Mita2000#"
+
+django_heroku.settings(locals())
