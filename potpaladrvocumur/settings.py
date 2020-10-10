058b7af1290146708ae74b0ec8d2d8ed2c28be7e
hanumansitaram <hanuman@unv.rs>
2020-10-10 22:35:21 +0200

    tweaks

--- potpaladrvocumur/settings.py
+++ potpaladrvocumur/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
-from decouple import config
+#import django_heroku
+#import dj_database_url
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
